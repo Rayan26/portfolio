ca140e8174016c69943e5cddc23c9ac6987fd697
Rk <kallabrayan@gmail.com>
2021-04-12 14:34:44 +0200

    Updates

---
+++ precache-manifest.98c923a6eabcc191622304e265cc96c6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a9329332ab910462c66b0049dc8ce5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "84a334990f249bbbc405",
+    "url": "/portfolio/static/css/main.b66896b0.chunk.css"
+  },
+  {
+    "revision": "caddc7a75a4552cc5f6d",
+    "url": "/portfolio/static/js/2.42125c39.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/portfolio/static/js/2.42125c39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a334990f249bbbc405",
+    "url": "/portfolio/static/js/main.ef975383.chunk.js"
+  },
+  {
+    "revision": "65bd4987f31e81c4403d",
+    "url": "/portfolio/static/js/runtime-main.a0093325.js"
+  },
+  {
+    "revision": "da37c8d3a68c7a7e870860fe5575e495",
+    "url": "/portfolio/static/media/CV.da37c8d3.pdf"
+  },
+  {
+    "revision": "6f82017092b3f62c335565b373a29482",
+    "url": "/portfolio/static/media/KronaOne-Regular.6f820170.ttf"
+  },
+  {
+    "revision": "37c8a1aaa2c1891aa0960ff03c377649",
+    "url": "/portfolio/static/media/Questrial-Regular.37c8a1aa.ttf"
+  }
+]);
